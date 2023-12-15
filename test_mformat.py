f24faa989deae8ec35882b296061ce7beddd1484
Jordan Dimov <jdimov@a115.co.uk>
2023-12-15 17:42:57 +0000

    Linting

--- test_mformat.py
+++ test_mformat.py
@@ -22,3 +22,31 @@ def test_for_months():
     assert mformat(date(2023, 2, 1), "M") == "Feb23"
     assert mformat(date(2023, 2, 19), "M") == "Feb23"
     assert mformat(date(2023, 12, 30), "M") == "Dec23"
+
+
+def test_for_years():
+    assert mformat(date(2022, 1, 5), "Y") == "2022"
+    assert mformat(date(2023, 2, 1), "Y") == "2023"
+    assert mformat(date(2023, 2, 19), "Y") == "2023"
+    assert mformat(date(2023, 12, 30), "Y") == "2023"
+
+
+def test_for_weeks():
+    assert mformat(date(2023, 1, 1), "W") == "W52-22"
+    assert mformat(date(2023, 1, 2), "W") == "W1-23"
+    assert mformat(date(2023, 1, 3), "W") == "W1-23"
+    assert mformat(date(2023, 1, 4), "W") == "W1-23"
+    assert mformat(date(2023, 1, 5), "W") == "W1-23"
+    assert mformat(date(2023, 1, 6), "W") == "W1-23"
+    assert mformat(date(2023, 1, 7), "W") == "W1-23"
+    assert mformat(date(2023, 1, 8), "W") == "W1-23"
+    assert mformat(date(2023, 1, 9), "W") == "W2-23"
+
+    assert mformat(date(2023, 12, 24), "W") == "W51-23"
+    assert mformat(date(2023, 12, 25), "W") == "W52-23"
+    assert mformat(date(2023, 12, 31), "W") == "W52-23"
+
+
+def test_for_unknown():
+    assert mformat(date(2023, 12, 31), "X") == "X"
+    assert mformat(date(2023, 12, 31), "unknown") == "unknown"
